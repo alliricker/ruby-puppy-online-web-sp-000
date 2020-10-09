ce0a76eec16198a8ad4dc628010aadb228bca26b
Allison Ricker <alliricker@gmail.com>
2020-10-09 03:01:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,39 @@
-# Add your code here
+class Dog
+attr_reader :name
+
+@@all = []
+
+def initialize (name)
+  @name = name
+  @@all << self
+  @save = save
+end
+
+def name
+  @name
+end
+
+
+def self.all
+  @@all
+end
+
+def self.print_all
+@@all.each do |names|
+  puts name
+end
+end
+
+def self.clear_all
+  @@all.clear
+end
+
+def save
+
+end
+
+
+
+end
+
+fido = Dog.new ("Fido")
